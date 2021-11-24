9d270fe36657676b95c2e85fd28c41355a3f0d19
Rob Donald <rob@statsresearch.co.uk>
2021-11-24 20:53:17 +0000

    String extract techniques. Adding folder to VS Code workspace

--- PhotosDB_List.py
+++ PhotosDB_List.py
@@ -9,8 +9,17 @@ phdb = osxphotos.PhotosDB(dbfile=file_to_list)
 
 print(phdb.keywords)
 
+all_keywords = phdb.keywords
+
 all_photos = phdb.photos()
 
 ernie = all_photos[26].exif_info
+iso = ernie.iso
+
+sb = []
+sb.append(",".join(str(x) for x in all_keywords))
+sb.append(str(iso))
+
+single_line_csv = ','.join(sb)
 
 print('Finished')
